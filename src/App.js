c8ffae945c1a15075c8ca2a3aef15e77c41392d7
Harold Beckerman <halbeckerman@yahoo.com>
2018-10-31 22:20:54 -0500

    Merge branch 'master' of https://github.com/Zestymcspicy/fendFinal

--- src/App.js
+++ src/App.js
@@ -22,12 +22,27 @@ class App extends Component {
        {name: "The Roost", location: {lat:38.9666393, lng: -95.235518}},
        {name: "Love Garden Sounds", location: {lat: 38.9684721, lng : -95.23553029999999}}
      ],
-     items : []
+     items : [],
+     myplaces : [],
+     mymarkers: [],
+     center: {},
+     zoom: 12
   }
 }
 componentDidMount() {
   foursquare.venues.getVenues(params)
       .then(res=> {
+        const myplaces = res.response;
+        console.log (myplaces)
+        const center = myplaces.geocode.feature.geometry.center;
+        const mymarkers = res.response.venues.map( venue => {
+          return {
+            lat: venue.location.lat,
+            lng: venue.location.lng,
+            name: venue.location.name
+          }
+        })
+        this.setState({mymarkers, myplaces, center})
         this.setState({ items: res.response.venues });
       });
   }
@@ -36,11 +51,11 @@ componentDidMount() {
     return (
       <div className="App">
         <header className="App-header">
-        LFK
-        </header>
-        <Menu places={this.state.items}/>
-          <Map
-          places={this.state.items}
+          LFK
+          </header>
+          <Menu places={this.state.items}/>
+          <Map className="LFKMap"
+          {...this.state}
         />
 
       </div>
--- src/Map.js
+++ src/Map.js
@@ -1,77 +1,29 @@
-import React, { Component } from  'react'
+import React, { Component } from 'react'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-var LFKMap;
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={props.zoom}
+    defaultCenter={props.center}
+  >
+    {props.mymarkers && props.mymarkers.map((place,index) =>
+      <Marker key={index} position={{lat:place.lat, lng:place.lng}} />
+  )}
+  </GoogleMap>
+))
 
 class Map extends Component {
-  constructor(props) {
-    super(props);
-    this.initMap = this.initMap.bind(this);    
-  }
-
-
-    markers = [];
-
-
-
-  async componentDidMount() {
-      window.initMap = this.initMap;
-      loadJS("https://maps.googleapis.com/maps/api/js?key=AIzaSyB63D2az3Guib3VGk7Auoie1fyG3lY1SzQ&v=3&callback=initMap")
-}
-
-
-  initMap = () => {
-    LFKMap = new window.google.maps.Map(document.getElementById('LFKMap'), {
-      center: {lat: 38.9717, lng: -95.2353},
-         zoom: 16
-    });
-    this.setInitialMarkers();
-    this.setMapBounds();
-  }
-
-  setInitialMarkers() {
-    for (var i = 0; i< this.props.places.length; i++) {
-      var position = this.props.places[i].location;
-      var title = this.props.places[i].title;
-
-      var marker = new window.google.maps.Marker({
-        position: position,
-        title: title,
-        map: window.LFKMap,
-        animation: window.google.maps.Animation.DROP,
-        id: i
-      });
-
-      this.markers.push(marker)
-    }
-  }
-
-  setMapBounds()  {
-    var bounds = new window.google.maps.LatLngBounds();
-    for (var i = 0; i < this.markers.length; i++) {
-      this.markers[i].setMap(LFKMap);
-      bounds.extend(this.markers[i].position);
-     }
-     LFKMap.fitBounds(bounds);
-   }
-
   render() {
     return (
-      <div className='map-container'>
-      <div id='LFKMap'></div>
-      </div>
-    )
-  }
-}
-
-
-//build the script element to attach the google maps api to
-function loadJS(src) {
-  let ref = window.document.getElementsByTagName('script')[0];
-  let script = window.document.createElement('script');
-  script.src = src;
-  script.async = true;
-  script.defer = true;
-  ref.parentNode.insertBefore(script, ref);
-}
-
+      <div className="my-map-container">
+      <MyMapComponent
+      isMarkerShown
+      googleMapURL="https://maps.googleapis.com/maps/api/js?&key=AIzaSyB63D2az3Guib3VGk7Auoie1fyG3lY1SzQ&v=3"
+      loadingElement={<div style={{ height: `100%`}} />}
+      containerElement={<div style={{ height: `100%`}} />}
+      mapElement={<div style={{ height: `100%` }} />}
+    />
+    </div>
+)
+}}
 export default Map;
